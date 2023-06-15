3a622dd0fa17009122520446b1434750648e86fb
UmrenTV <zdravevski.jane@gmail.com>
2023-06-15 13:16:41 +0200

    Started working on products edit functionality. TODO: Prepopulate the edit fields with data

---
+++ MainProject/models/cart.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "cart.json"
+);
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    // Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      // Add new product/ increase quanitty
+      let updatedProduct;
+      if (existingProduct) {
+        console.log("EXISTING PRODUCT FOUND!");
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice; // the + in front of productPrice is to convert it to number
+      console.log("CART", cart);
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log("WRITING ERROR", err);
+      });
+    });
+  }
+};
--- MainProject/routes/shop.js
+++ MainProject/routes/shop.js
@@ -1,19 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const shopController = require('../controllers/shop');
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
+router.get("/", shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
-router.get('/cart', shopController.getCart);
+// router.get(/products/delete) - this is how you define it instead. Read more below.
 
-router.get('/orders', shopController.getOrders);
+router.get("/products/:productId", shopController.getProduct);
 
-router.get('/checkout', shopController.getCheckout);
+// router.get('/products/delete', shopController.getProduct); - it is important where you define the routes here. If you put /products/delete, it will not work because it's below /products/:productId and it will not work because the code is executed top to bottom, which means /product/delete belongs to the /product/:whatever, whatever being delete as well. So the delete will be handled by the /product/:productId route and be considered as dynamic parameter instead of actual route.
+
+router.get("/cart", shopController.getCart);
+
+router.post("/cart", shopController.postCart);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
